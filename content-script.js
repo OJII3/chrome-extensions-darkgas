147f64b2ece2928159cfd5ecf166e3d718ec463e
OJII3 <kawaisounayuriko@gmail.com>
2022-05-15 19:08:19 +0900

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,27 +1,35 @@
 'use strict';
 
+const buttonSpace = document.querySelector('div.INSTk');
+
 const body = document.getElementsByTagName('body')[0];
 const header = document.getElementsByClassName('gb_Md')[0];
 const side_bar = document.getElementsByClassName('td5WLe')[0];
-const editer = document.getElementsByClassName('monaco-mouse-cursor-text')[0];
-
-const gray_font = [
-    document.getElementsByClassName('gb_5d')[0],
-    document.getElementsByClassName('UGZzee KOiXb')[0]
-];
-const baseFormat = document;
+const editors = document.querySelectorAll('.OX2gTc, .uZSCEf, .monaco-editor, .monaco-editor-background, .monaco-editor .inputarea.ime-input');
+const gray_font_elements = document.querySelectorAll('.gb_5d, .UGZzee, .KOiXb');
+let mainButton;
 
 const changeTheme = theme => {
-    body.style = "background-color: #111; color: #fff;";
-    header.style = "background-color: #000; color: #fff;";
-    side_bar.style = "background-color: #000; color: #fff;";
-    //editer.style = "background-color: #000;";
 
-    for (const gray of gray_font) {
-        gray.style = "color: #ccc;";
+    body.style = "background-color: #111; color: #fff;";
+    header.style = "background-color: #222; color: #fff;";
+    side_bar.style = "background-color: #000; color: #fff;";
+    
+    for (const element of editors) {
+        element.style = "background-color: #000;";
+    }
+
+    for (const element of gray_font_elements) {
+        element.style = "color: #ccc;";
     }
 };
 
-window.onload = changeTheme('dark');
+const createMainButton = () => {
+    mainButton = document.createElement('button');
+    mainButton.id = 'main_button';
+    mainButton.innerHTML = "on";
+    mainButton.addEventListener('click', changeTheme());
+    buttonSpace.appendChild(mainButton);
+};
 
-
+createMainButton();
